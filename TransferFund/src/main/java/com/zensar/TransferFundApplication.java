77bcfacdfbc1005c29bd866b4ea73f9a38f8dfe5
RK65964 <RK65964@AESW42SLPC4458.ind.zensar.com>
2022-02-21 11:05:36 +0530

    Commit for Transfer fund

---
+++ TransferFund/src/main/java/com/zensar/TransferFundApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferFundApplication.class, args);
+	}
+
+}
